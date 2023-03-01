0c15c29a2d9cd17edba0188a3eb8b0351ab7e95b
Nasim ghalebi <n.ghalebi@gmail.com>
2023-03-01 15:51:25 +0100

    add HomePage.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container, Button, Row} from 'react-bootstrap'
+
+
+const HomePage = () => {
+  return (
+    <div>
+        <Container>
+          <Row>
+            <img src='../public/images/airplan1.png' alt='airplan'/>
+          </Row> 
+            <span>Let’s travel the beautiful world together</span>
+
+            <Button variant="primary" >Sign in</Button>
+            <a href='#'>Create an account</a>
+        </Container>
+      
+    </div>
+  )
+}
+
+
+export default HomePage
